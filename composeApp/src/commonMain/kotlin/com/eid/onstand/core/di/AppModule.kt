64bc969c8fc3fc3afda1181696dc12b9a95179da
mohamedsaber <mohamedsaber09@gmail.com>
2025-07-25 00:31:50 +0200

    refactor: Remove old repository and ViewModel architecture
    - Remove AppViewModel and PreviewViewModel
    - Remove BackgroundRepository, ClockRepository, and CustomizationRepository
    - Remove old CustomizationScreen and HomeScreen components
    - Remove CustomizationOptions and related preview components
    - Remove BackgroundTypeConverter utility
    - Update AppModule to remove old dependencies
    - Clean up legacy MVVM pattern in favor of registry system

--- composeApp/src/commonMain/kotlin/com/eid/onstand/core/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/com/eid/onstand/core/di/AppModule.kt
@@ -1,21 +1,10 @@
 package com.eid.onstand.core.di
 
-import com.eid.onstand.AppViewModel
-import com.eid.onstand.core.data.BackgroundRepository
-import com.eid.onstand.core.data.ClockRepository
-import com.eid.onstand.core.data.CustomizationRepository
 import com.eid.onstand.core.data.CustomizationDataSource
 import com.eid.onstand.core.data.createDataStore
-import com.eid.onstand.feature.preview.PreviewViewModel
-import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
     single { createDataStore() }
     single { CustomizationDataSource(get()) }
-    single { BackgroundRepository() }
-    single { ClockRepository() }
-    single { CustomizationRepository(get(), get(), get()) }
-    viewModel { AppViewModel(get()) }
-    viewModel { PreviewViewModel(get()) }
 }
